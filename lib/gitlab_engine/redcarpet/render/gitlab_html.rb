de4d21633686fa73b494a569a5b5fb89e8088e7a
John Long <john.long@ors.sc.gov>
2012-08-06 16:00:29 -0400

    try to prevent errors from python lexer

--- lib/gitlab_engine/redcarpet/render/gitlab_html.rb
+++ lib/gitlab_engine/redcarpet/render/gitlab_html.rb
@@ -1,9 +1,9 @@
 class Redcarpet::Render::GitlabHTML < Redcarpet::Render::HTML
   def block_code(code, language)
     if Pygments::Lexer.find(language)
-      Pygments.highlight(code, :lexer => language, :options => {:encoding => 'utf-8'})
+      Pygments.highlight(code, :lexer => language, :options => {:encoding => 'utf-8'}) rescue code
     else
-      Pygments.highlight(code, :options => {:encoding => 'utf-8'})
+      Pygments.highlight(code, :options => {:encoding => 'utf-8'}) rescue code
     end
   end
 end
